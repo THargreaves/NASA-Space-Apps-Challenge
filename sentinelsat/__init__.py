349c5af11630173377896c02f18679bfc99857df
Matthew Bardsley <Matthew.Bardsley@warwick.ac.uk>
2020-10-04 10:49:57 +0100

    Some other dependencies

---
+++ sentinelsat/__init__.py
@@ -0,0 +1,21 @@
+__version__ = "0.14"
+
+# Import for backwards-compatibility
+from . import sentinel
+from .exceptions import (
+    SentinelAPIError,
+    SentinelAPILTAError,
+    ServerError,
+    InvalidKeyError,
+    QueryLengthError,
+    QuerySyntaxError,
+    UnauthorizedError,
+    InvalidChecksumError,
+)
+from .sentinel import (
+    SentinelAPI,
+    format_query_date,
+    geojson_to_wkt,
+    read_geojson,
+    placename_to_wkt,
+)
